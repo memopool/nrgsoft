1e1bdaf89294945141262b30ffb69f81460d1f59
dokugo <7773972+dokugo@users.noreply.github.com>
2020-05-13 23:58:12 +0300

    Refactor reducer utils
    * Decouple checkIfTarget function form updateArrayElement function
    * Remove obsolete spread operators

--- src/store/reducers/utils.js
+++ src/store/reducers/utils.js
@@ -1,20 +1,13 @@
 export const addArrayElement = (state, payload) => [...state, payload.post]
 
-export const updateArrayElement = (state, payload) => {
-  const checkIfTarget = element => {
-    if (element.id === payload.id) {
-      return { ...element, isLiked: !element.isLiked }
-    }
+const toggleLike = (element, targetId) =>
+  element.id === targetId ? { ...element, isLiked: !element.isLiked } : element
 
-    return element
-  }
+export const updateArrayElement = (state, payload) =>
+  state.map(element => toggleLike(element, payload.id))
 
-  return [...state.map(element => checkIfTarget(element))]
-}
-
-export const removeArrayElement = (state, payload) => [
-  ...state.filter(element => element.id !== payload.id),
-]
+export const removeArrayElement = (state, payload) =>
+  state.filter(element => element.id !== payload.id)
 
 export const moveArrayElement = (state, payload) => {
   const { startIndex, endIndex } = payload.indexes
